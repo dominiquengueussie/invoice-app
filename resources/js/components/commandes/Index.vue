f359eeb94b12e4dd38eaf7a47223d96924bc8665
YEMDOM Dominique <dominique@POLYPHARMA.local>
2023-11-16 17:33:17 +0100

    affichage des infos de commande

--- resources/js/components/commandes/Index.vue
+++ resources/js/components/commandes/Index.vue
@@ -57,13 +57,17 @@
                 </div>
 
                 <!-- item 1 -->
-                <div class="table--items">
-                    <a href="#" class="table--items--transactionId">#093654</a>
-                    <p>Jan 18, 9:31am</p>
-                    <p>#093654</p>
-                    <p>Jonathan Yu</p>
-                    <p>Jan 18, 9:31am</p>
-                    <p> $ 16,943</p>
+                <div class="table--items" v-for="item in invoices" :key="item.id" v-if="invoices.length > 0">
+                    <a href="#" class="table--items--transactionId">#{{ item.id }}</a>
+                    <p>{{ item.date }}</p>
+                    <p>#{{item.number}}</p>
+                    <p>{{ item.customer_id }}</p>
+                    <p>{{ item.date_echeance }}</p>
+                    <p>{{ item.total }} fcfa</p>
+                </div>
+
+                <div style="text-align: center;" v-else>
+                   aucune commande trouvé !
                 </div>
             </div>
         </div>
@@ -82,7 +86,8 @@ onMounted(async () => {
 
 const getInvoices = async () => {
     let response = await axios.get('api/get_all_invoice')
-    console.log('response', response);
+    //console.log('response', response);
+    invoices.value = response.data.invoices
 }
 
 </script>
